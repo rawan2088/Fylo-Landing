5379550c9adc709d5e69d449f87fe2d309cb28e1
rawan2088 <rawan.ahmed.2006.10@gmail.com>
2023-03-25 13:45:48 +0200

    'new'

---
+++ main.js
@@ -0,0 +1,6 @@
+// let width = document.querySelector(".wavy img");
+let wave = document.getElementsByClassName("wavy")[0];
+
+let need = wave.clientHeight;
+console.log(need);
+// console.log(wave.height);
